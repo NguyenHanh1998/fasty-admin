9d3055386ed7ec8392aa146cefdd26c302df3876
Hanh Nguyen <hanh.nguyen2@sotatek.com>
2021-04-04 22:46:22 +0700

    feat: add css anf app container

---
+++ src/views/dashboard/components/AppContainer.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="cards__container-wrapper">
+    <div
+      v-if="!!headerTitle"
+      class="cards__header"
+    >
+      {{ headerTitle }}
+    </div>
+
+    <div
+      class="cards__main-layout"
+      :class="{
+        'cards__auto-height': autoHeight
+      }"
+    >
+      <div class="cards__coins-container">
+        <slot name="coins-content" />
+      </div>
+
+      <div
+        class="cards__container"
+        :class="containerClass"
+      >
+        <slot name="content" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppContainer',
+
+    props: {
+      headerTitle: {
+        type: String,
+        default: '',
+      },
+      autoHeight: {
+        type: Boolean,
+        default: false,
+      },
+      containerClass: {
+        type: String,
+        default: '',
+      },
+    },
+
+    data () {
+      return {
+
+      }
+    },
+
+    mounted () {
+
+    },
+
+    methods: {
+
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
